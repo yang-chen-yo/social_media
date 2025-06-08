07e2eec3ec4091480cac8d6bc31c0fd795fdf59a
yang-chen-you <boy7770730@mail.com>
2025-06-08 18:45:10 +0800

    Done

--- main.py
+++ main.py
@@ -38,6 +38,6 @@ if __name__ == '__main__':
         db.create_all()
     try:
         print("Flask app starting at http://127.0.0.1:5000")
-        app.run(debug=True)
+        app.run(host="0.0.0.0", port=5000, debug=False)
     except Exception as e:
         print(f"[ERROR] Failed to start app: {e}")
